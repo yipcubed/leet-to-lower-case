a51249931ff55518ef5c0b8225b9c5f05f9c2920
Yip Cubed <yipcubed@gmail.com>
2019-08-07 12:53:59 -0700

    starting

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+
+
+#include "../utils/VectorUtils.hpp"
+#include "../utils/PrintUtils.hpp"
+// #include "../utils/HeapUtils.hpp"
+// #include "../utils/BinarySearch.hpp"
+// #include "../utils/TreeUtils.hpp"
+
+using namespace std;
+
+// https://leetcode.com/problems/to-lower-case/
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
+// Live coding problems, watch at
+// https://www.twitch.tv/yipcubed
+// https://www.youtube.com/channel/UCTV_UOPu7EWXvYWsBFxMsSA/videos
+//
+
+// makes code faster, but larger. Just for LeetCode fun!
+#pragma GCC optimise ("Ofast")
+
+// makes stdin not synced so it is faster. Just for LeetCode fun!
+static auto __ __attribute__((unused)) = []() {              // NOLINT
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    string toLowerCase(string& str) {
+        static constexpr int diff = 'a' - 'A';
+        for (auto& c: str) {
+            if (c >= 'A' && c <= 'Z')
+                c += diff;
+        }
+        return str;
+    }
+};
+
+void test1() {
+    string s = "HZAhello";
+
+    cout << " ? " << Solution().toLowerCase(s) << endl;
+}
+
+void test2() {
+
+}
+
+void test3() {
+
+}
